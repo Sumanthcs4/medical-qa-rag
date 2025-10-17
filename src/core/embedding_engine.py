991289e53c459e900592c6830160c39305a7965f
Sumanthcs4 <ssumanth510@gmail.com>
2025-10-17 19:50:32 +0530

    some errors were corrected

--- src/core/embedding_engine.py
+++ src/core/embedding_engine.py
@@ -1,15 +1,21 @@
 import os, sys 
-from transformers import sentence_transformers
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+
 from config import EMBEDDING_MODEL_NAME
-from utils.logger import setup_logger
+from src.utils.logger import setup_logger
 logger = setup_logger()
 class EmbeddingEngine:
     def __init__(self, model_name=EMBEDDING_MODEL_NAME):
-        self.model = sentence_transformers.SentenceTransformer(model_name)
+        # Correctly use the imported class directly
+        self.model = SentenceTransformer(model_name) # <- Correct
         logger.info(f"Loaded embedding model: {model_name}")
+
     def generate_embedding(self, text_chunks):
-        return self.model.encode(text_chunks)
+        embeddings = self.model.encode(text_chunks)
         logger.info(f"Generated embeddings for {len(text_chunks)} text chunks")
+        return embeddings
+        
         
         
         
